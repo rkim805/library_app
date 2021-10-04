16bd7349a22678bbde9cdd8c49a9f7b5151f6bab
Ryan Kim <rkim805@gmail.com>
2021-10-04 14:09:10 -0700

    Create local storage persistance
    App now checks to see if local storage is usable with the user's
    browser settings.
    
    Library checks local storage on load to see if data from prior
    session exists. Local storage is updated on create, update, and delete
    operations.
    
    When recreating the myLibrary from local storage, the Object obtained
    from parsing the JSON string must be re-processed such that the objects
    corresponding to map keys are Books. This allows continued use of
    toggleRead() after reloading the page.
    
    Misc. Changes:
      -- Book constructor refactored to factory function in order to
         prevent common constructor-pattern errors(i.e. calling constructor
         without new operator).

--- js/libapp.js
+++ js/libapp.js
@@ -1,7 +1,12 @@
+'use strict';
 window.onload = initLibrary();
 
 function initLibrary () {
-  let myLibrary = new Map();
+  let myLibrary = recreateMap();
+  if(myLibrary == undefined) {
+    myLibrary = new Map();
+  }
+
   displayBooks();
   let addBtn = document.querySelector("#add-btn");
   let closeBtn = document.querySelector("#close-btn");
@@ -12,17 +17,17 @@ function initLibrary () {
   submitBtn.addEventListener("click", handleSubmit);
   window.addEventListener("click", modalFormHandler);
 
-  function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.hash = "";
+  function Book(title, author, pages, read, hash="") {
+    const book = Object.create(Book.prototype);
+    book.title = title;
+    book.author = author;
+    book.pages = pages;
+    book.read = read;
+    book.hash = hash;
+    return book;
   }
 
-  Book.prototype.toggleRead = function() {
-    this.read = !(this.read);
-  }
+  Book.prototype.toggleRead = createProtoFunction();
 
   //ensure property is not displayed when properties are looped through
   Object.defineProperty(Book.prototype, "toggleRead", {
@@ -83,10 +88,10 @@ function initLibrary () {
     const key =  event.target.parentNode.getAttribute("data-key");
     myLibrary.delete(key);
     displayBooks();
+    updateLocalStorage();
   }
 
   function createToggleReadBtn(readStatus) {
-    console.log(readStatus);
     const toggleBtn = document.createElement("Button");
     if(readStatus) {
       toggleBtn.textContent = "Read"
@@ -115,6 +120,7 @@ function initLibrary () {
     const key = this.parentNode.getAttribute("data-key");
     const book = myLibrary.get(key);
     book.toggleRead();
+    updateLocalStorage();
   }
 
   function handleDisplayForm() {
@@ -156,7 +162,7 @@ function initLibrary () {
           }
         }
       }
-      const newBook = new Book(...propsArr);
+      const newBook = Book(...propsArr);
       const hash = createHash(newBook);
       if(myLibrary.has(hash)) {
         alert("This book has already been added to the library! " +
@@ -168,11 +174,70 @@ function initLibrary () {
         closeModal();
         displayBooks();
       }
+      updateLocalStorage();
     }
   }
 
   function createHash(book) {
     const hashVal = objectHash.sha1(book);
     return hashVal;
+  }
+
+  function createProtoFunction() {
+    const toggle = function() {
+      this.read = !(this.read);
+    };
+    return toggle;
+  }
+
+  function localStorageUsable() {
+    let test = "test";
+    try {
+      localStorage.setItem(test, test);
+      localStorage.removeItem(test);
+      return true;
+    } catch(e) {
+      return false;
+    }
+  }
+
+  function updateLocalStorage() {
+    if(localStorageUsable()) {
+      console.log(myLibrary);
+      let libJSON = JSON.stringify([...myLibrary]);
+      localStorage.setItem("myLibrary", libJSON);
+    }
+  }
+
+  function recreateMap() {
+    let libJSON = localStorage.getItem("myLibrary");
+    if(libJSON == undefined) {
+      return libJSON;
+    }
+    let libObj = recreateBooks(JSON.parse(libJSON));
+    let retMap = new Map(libObj);
+    return retMap;
+  }
+
+  /**
+   * recreateBooks()
+   * 
+   * This function is designed to take objects from the parsed JSON
+   * and turn them from generic objects to Book objects. This is to
+   * re-enable any prototype functions defined on these Book objects.
+   * 
+   * @param {Object} libObj -- Object obtained from parsing library from
+   * localStorage. libObj[i][0] contains key/hash, libObj[i][1] contains
+   * object to be converted to a book.
+   * 
+   * @returns libObj -- Object with Book objects in place of generic objects.
+   */
+  function recreateBooks(libObj) {
+    for(let i = 0; i < libObj.length; i++) {
+      let objToConvert = libObj[i][1]
+      libObj[i][1] = Book(objToConvert.title, objToConvert.author, 
+        objToConvert.pages, objToConvert.read, objToConvert.hash);
+    }
+    return libObj;
   }
 }
